a017f7fdfef2119ec553942152cf968abe831ccf
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-11 14:49:25 +0600

    added db user and password to uri

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors({
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USER_DB}:<password>@databases1.utppk3d.mongodb.net/?retryWrites=true&w=majority&appName=databases1`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@databases1.utppk3d.mongodb.net/?retryWrites=true&w=majority&appName=databases1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
