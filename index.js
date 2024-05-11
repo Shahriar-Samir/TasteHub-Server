5bc78d7a3b2dd25ffb908cf296d5e1907955aef3
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-11 14:57:53 +0600

    added allFoods route to get all the food items from database

--- index.js
+++ index.js
@@ -29,6 +29,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    
+    const foodItemsCollection = client.db('Restaurant_Management').collection('AllFoodItems')
+
+    app.get('/allFoods',async (req,res)=>{
+        const allFoods = await foodItemsCollection.find().toArray()
+        res.send(allFoods)
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
