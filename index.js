f4098425b0dd43634127b649f1f00ebc99b8e481
Shahriar-Samir <shabusiness035@gmail.com>
2024-05-12 04:16:01 +0600

    added a get route for specific food item details

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors({
 }))
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@databases1.utppk3d.mongodb.net/?retryWrites=true&w=majority&appName=databases1`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -36,6 +36,12 @@ async function run() {
     app.get('/allFoods',async (req,res)=>{
         const allFoods = await foodItemsCollection.find().toArray()
         res.send(allFoods)
+    })
+
+    app.get('/foodDetails/:id',async (req,res)=>{
+        const {id} = req.params
+        const foodItem = await foodItemsCollection.findOne({_id: new ObjectId(id)})
+        res.send(foodItem)
     })
 
     app.get('/myFoods/:email',async (req,res)=>{
